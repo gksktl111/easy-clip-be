33fd302a43859184083efc64c3dc2413ddb271ac
gksktl111 <gksktl111@naver.com>
2026-01-26 03:15:17 +0900

    feat(clip): 폴더 목록 조회 API 추가

--- src/folders/folders.controller.ts
+++ src/folders/folders.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Post, Request, UseGuards } from '@nestjs/common';
+import { Body, Controller, Get, Post, Request, UseGuards } from '@nestjs/common';
 import { JwtAccessGuard } from 'src/auth/guards/jwt-access-token.guard';
 import { JwtPayload } from 'src/auth/auth';
 import { CreateFolderDto } from './dtos/create-folder.dto';
@@ -7,6 +7,12 @@ import { FoldersService } from './folders.service';
 @Controller('folders')
 export class FoldersController {
   constructor(private foldersService: FoldersService) {}
+
+  @Get()
+  @UseGuards(JwtAccessGuard)
+  getFolders(@Request() req: { user: JwtPayload }) {
+    return this.foldersService.getFolders(req.user.sub);
+  }
 
   @Post()
   @UseGuards(JwtAccessGuard)
