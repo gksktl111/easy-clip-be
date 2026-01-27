1e581a4181de2153a835c8d3da6f5cb76fcdb208
minku <50075024+gksktl111@users.noreply.github.com>
2026-01-27 17:49:54 +0900

    feat: 폴더 클립 무한스크롤 조회 엔드포인트 추가 (#23)
    * feat(clip): 폴더 클립 무한스크롤 조회 엔드포인트 추가
    
    * fix : 테스트 코드 수정

--- src/users/users.service.spec.ts
+++ src/users/users.service.spec.ts
@@ -13,10 +13,7 @@ describe('UsersService', () => {
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [
-        UsersService,
-        { provide: PrismaService, useValue: prisma },
-      ],
+      providers: [UsersService, { provide: PrismaService, useValue: prisma }],
     }).compile();
 
     service = module.get<UsersService>(UsersService);
